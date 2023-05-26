7a265f655d72978fd173263b22a138ee05aa9be0
Dilandds <e17379@eng.pdn.ac.lk>
2023-05-26 21:07:27 +0600

    completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,20 +11,36 @@ import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
 import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
-import { action as manipulateEventAction } from "./components/EventForm";
+import {
+  action,
+  action as manipulateEventAction,
+} from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage from "./pages/Authentication";
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
+import { tokenLoader } from "./util/auth";
+
+import { action as logoutAction } from "./pages/Logout";
+import { getAuthToken } from "./util/auth";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    id: "root",
+    loader: tokenLoader,
     children: [
       { index: true, element: <HomePage /> },
       {
         path: "auth",
         element: <AuthenticationPage />,
+        action: authAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
       },
       {
         path: "events",
@@ -56,6 +72,7 @@ const router = createBrowserRouter([
             path: "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
+            loader: getAuthToken,
           },
         ],
       },
